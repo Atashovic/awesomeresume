b0e43acb6bee7508913be3053ad56f459eeb6cb9
Jabir Amirkhanli <cabiramirxanli@gmail.com>
2020-11-23 09:06:53 +0400

    Release on 23.11.2020

---
+++ ResumeDesktopApp01112020/src/main/java/cm/company/resume/config/Config.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm.company.resume.config;
+
+import com.company.entity.User;
+
+/**
+ *
+ * @author jabir.amirkhanli
+ */
+public class Config {
+    
+    public static volatile User loggedInUser;
+    
+}
